7f608c3535efdcd97812bcacb599999bc33a03a2
skyne98 <ahalahan@gmail.com>
2024-05-09 20:37:30 +0200

    feat: boa, vuejs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,52 @@
+use boa_engine::{context::ContextBuilder, js_string, property::Attribute, Source};
+use boa_runtime::Console;
 use eframe::egui;
 use eyre::{Context, Result};
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+
+    // Set up the JS virtual machine
+    let vue_code = include_str!("../assets/vue.global.js");
+
+    // Instantiate the execution context
+    let mut context = ContextBuilder::default()
+        .build()
+        .expect("Building the default context should not fail");
+
+    // Add the `console.log` function to the context
+    let console = Console::init(&mut context);
+    context
+        .register_global_property(js_string!(Console::NAME), console, Attribute::all())
+        .expect("the console object shouldn't exist");
+
+    // Parse the source code
+    match context.eval(Source::from_bytes(vue_code)) {
+        Ok(res) => {
+            println!(
+                "{}",
+                res.to_string(&mut context).unwrap().to_std_string_escaped()
+            );
+        }
+        Err(e) => {
+            // Pretty print the error
+            eprintln!("Uncaught {e}");
+        }
+    };
+
+    // Try to initialize the Vue app
+    match context.eval(Source::from_bytes("Vue.createApp({}).mount('#app')")) {
+        Ok(res) => {
+            println!(
+                "{}",
+                res.to_string(&mut context).unwrap().to_std_string_escaped()
+            );
+        }
+        Err(e) => {
+            // Pretty print the error
+            eprintln!("Uncaught {e}");
+        }
+    };
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
