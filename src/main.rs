210880e17161c04af22942f178710f7a01164672
raspbfox <ahalahan@gmail.com>
2024-05-10 06:56:04 +0200

    feat: reactivity, events

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,98 @@ use eframe::egui;
 use eyre::{Context, ContextCompat, Result};
 use rusty_v8 as v8;
 
+use colored::*;
+
+fn value_to_string<'s>(
+    scope: &mut v8::HandleScope<'s>,
+    value: v8::Local<v8::Value>,
+    depth: usize,
+) -> Result<String> {
+    const INDENT: &str = "  "; // Two spaces for each indentation level
+    let indentation = INDENT.repeat(depth);
+    let value_indentation = INDENT.repeat(depth + 1); // Increase indent for values
+
+    if value.is_function() {
+        let function = v8::Local::<v8::Function>::try_from(value).unwrap();
+        let function_source = function
+            .to_string(scope)
+            .wrap_err("Failed to convert function to string")?
+            .to_rust_string_lossy(scope);
+
+        Ok(format!(
+            "{} {}",
+            "ƒ".bright_magenta(),
+            function_source.bright_cyan(),
+        ))
+    } else if value.is_array() {
+        let array = v8::Local::<v8::Array>::try_from(value).unwrap();
+        let length = array.length();
+        let mut result = String::new();
+
+        result.push_str(&format!("{}[\n", indentation.cyan()));
+
+        for i in 0..length {
+            let js_index = v8::Integer::new(scope, i as i32);
+            let element_value = array.get(scope, js_index.into()).unwrap();
+            let element_str = value_to_string(scope, element_value, depth + 1)?;
+
+            result.push_str(&format!("{}{},\n", value_indentation.cyan(), element_str));
+        }
+
+        result.push_str(&format!("{}]", indentation.cyan())); // Closing bracket with base indentation
+
+        Ok(result)
+    } else if value.is_object() {
+        let object = value.to_object(scope).unwrap();
+        let property_names = object.get_property_names(scope).unwrap();
+        let mut result = String::new();
+
+        result.push_str(&format!("{{\n{}", indentation.cyan()));
+
+        for i in 0..property_names.length() {
+            let js_index = v8::Integer::new(scope, i as i32);
+            let key = property_names.get(scope, js_index.into()).unwrap();
+            let key_str = key.to_string(scope).unwrap().to_rust_string_lossy(scope);
+            let property_value = object.get(scope, key.into()).unwrap();
+            let property_str = value_to_string(scope, property_value, depth + 1)?;
+
+            result.push_str(&format!(
+                "{}{}: {},\n",
+                value_indentation.cyan(),
+                key_str.green(),
+                property_str
+            ));
+        }
+
+        result.push_str(&format!("{}}}", indentation.cyan())); // Closing brace with base indentation
+
+        Ok(result)
+    } else {
+        let maybe_string = value.to_string(scope);
+        let string = maybe_string
+            .context("Failed to convert value to string")?
+            .to_string(scope)
+            .wrap_err("Failed to convert value to string")?;
+        Ok(string.to_rust_string_lossy(scope))
+    }
+}
+
 fn log_function(
     scope: &mut v8::HandleScope,
     args: v8::FunctionCallbackArguments,
     rv: v8::ReturnValue,
 ) {
-    for i in 0..args.length() {
+    let args_len = args.length();
+    let mut result = String::new();
+    for i in 0..args_len {
         let arg = args.get(i);
-        let arg = arg.to_string(scope).unwrap();
-        let arg = arg.to_rust_string_lossy(scope);
-        println!("{}", arg);
+        let arg = value_to_string(scope, arg, 0).unwrap();
+        result.push_str(&arg);
+        if i < args_len - 1 {
+            result.push_str(", ");
+        }
     }
+    println!("{}", result);
 }
 // hook up a named function to the console object
 fn hook_function(
@@ -43,37 +124,44 @@ async fn main() -> Result<()> {
 
     {
         let isolate = &mut v8::Isolate::new(v8::CreateParams::default());
-        let handle_scope = &mut v8::HandleScope::new(isolate);
-        let context = v8::Context::new(handle_scope);
-        let scope = &mut v8::ContextScope::new(handle_scope, context);
+        isolate.set_microtasks_policy(v8::MicrotasksPolicy::Explicit);
+        {
+            let handle_scope = &mut v8::HandleScope::new(isolate);
+            let context = v8::Context::new(handle_scope);
+            let scope = &mut v8::ContextScope::new(handle_scope, context);
 
-        let global = context.global(scope);
-        let console_name =
-            v8::String::new(scope, "console".into()).wrap_err("Failed to create string")?;
-        let console = v8::Object::new(scope);
-        global
-            .set(scope, console_name.into(), console.into())
-            .wrap_err("Failed to set console object")?;
-        // Set up the console functions
-        hook_function(scope, console, "log", log_function)?;
-        hook_function(scope, console, "info", log_function)?;
-        hook_function(scope, console, "warn", log_function)?;
-        hook_function(scope, console, "error", log_function)?;
+            let global = context.global(scope);
+            let console_name =
+                v8::String::new(scope, "console".into()).wrap_err("Failed to create string")?;
+            let console = v8::Object::new(scope);
+            global
+                .set(scope, console_name.into(), console.into())
+                .wrap_err("Failed to set console object")?;
+            // Set up the console functions
+            hook_function(scope, console, "log", log_function)?;
+            hook_function(scope, console, "info", log_function)?;
+            hook_function(scope, console, "warn", log_function)?;
+            hook_function(scope, console, "error", log_function)?;
 
-        // Set up the JS virtual machine
-        let vue_code = include_str!("../assets/vue.global.js");
-        let vue_code = v8::String::new(scope, vue_code).wrap_err("Failed to create JS string")?;
-        let script =
-            v8::Script::compile(scope, vue_code, None).wrap_err("Failed to compile script")?;
-        let result = script.run(scope).wrap_err("Failed to run script")?;
-        let result = result
-            .to_string(scope)
-            .wrap_err("Failed to convert result to string")?;
-        println!("result (vue init): {}", result.to_rust_string_lossy(scope));
+            // Set up the JS virtual machine
+            let vue_code = include_str!("../assets/vue.global.js");
+            let vue_code =
+                v8::String::new(scope, vue_code).wrap_err("Failed to create JS string")?;
+            let script =
+                v8::Script::compile(scope, vue_code, None).wrap_err("Failed to compile script")?;
+            let result = script.run(scope).wrap_err("Failed to run script")?;
+            let result = result
+                .to_string(scope)
+                .wrap_err("Failed to convert result to string")?;
+            println!("result (vue init): {}", result.to_rust_string_lossy(scope));
 
-        // Try to initialize the Vue app
-        let vue_init_code = r#"
-    const { createRenderer, defineComponent, h } = Vue;
+            // Try to initialize the Vue app
+            let vue_init_code = r#"
+    const { createRenderer, defineComponent, h, reactive } = Vue;
+
+    const globalState = reactive({
+        a: 1
+    });
 
     const nodeOps = {
         // Create a node in the non-DOM environment
@@ -105,36 +193,88 @@ async fn main() -> Result<()> {
             el.children = [{ type: 'text', text }];
         },
         patchProp(el, key, prevValue, nextValue) {
-            console.log(`Patching prop: ${key} from ${prevValue} to ${nextValue}`);
+            let nextValueType = typeof nextValue;
+            console.log(`Patching prop: ${key} from ${prevValue} to ${nextValue} of type ${nextValueType}`);
             el.attributes[key] = nextValue;
-        }
+        },
+        parentNode(node) {
+            console.log(`Getting parent node of: ${node.tag}`);
+            // Return the parent node
+            return node;
+        },
+        nextSibling(node) {
+            console.log(`Getting next sibling of: ${node.tag}`);
+            // Return the next sibling node
+            return node;
+        },
+        querySelector(selector) {
+            console.log(`Querying selector: ${selector}`);
+            // Return the first element that matches the selector
+            return { tag: 'div', children: [], attributes: {} };
+        },
     };
 
     const { render, createApp } = createRenderer(nodeOps);
+    const { ref } = Vue;
 
     const App = {
         setup() {
-            const { ref, computed } = Vue;
+            console.log('App setup:');
+            const { ref, computed, nextTick } = Vue;
 
-            const a = ref(1);
             const b = ref(2);
-            const sum = computed(() => a.value + b.value);
+            const sum = computed(() => {
+                console.log(`Computing sum: ${globalState.a} + ${b.value}`);
+                return globalState.a + b.value;
+            });
 
-            return { sum };
+            // mount and unmount lifecycle hooks
+            Vue.onMounted(() => {
+                console.log('App mounted:');
+                console.log(`a: ${globalState.a}`);
+                console.log(`b: ${b.value}`);
+            });
+            Vue.onUnmounted(() => {
+                console.log('App unmounted:');
+                console.log(`a: ${globalState.a}`);
+                console.log(`b: ${b.value}`);
+            });
+
+            function changeB() {
+                b.value = 3;
+            }
+
+            nextTick(() => {
+                console.log('Next tick:');
+                console.log(`a: ${globalState.a}`);
+                console.log(`b: ${b.value}`);
+            });
+
+            return { b, sum, changeB };
         },
-        template: '<div id="main" style="color: red"><span>Hello, sum is: {{ sum }}</span></div>',
+        template: '<div id="main" style="color: red"><span @click="() => 2" @fire="() => 2">Hello, sum is: {{ sum }}</span></div>',
     };
 
     // The 'root' object would represent the top level of your app
     const root = { tag: 'app', children: [], attributes: {} };
     console.log(`Root object created: ${JSON.stringify(root, null, 2)}`);
-    createApp(App).mount(root);
-    console.log('App mounted');
+    const appInstance = createApp(App).mount(root);
+
+    // Update a and b values
+    console.log(appInstance);
+    globalState.a = 2;
+    appInstance.changeB();
 
     // For demonstration, log the root object to see the structure
     function circularReplacer() {
         const seen = new WeakSet();
         return (key, value) => {
+            if (key === '_vnode') {
+                return '[VNode]'; // Indicate virtual node
+            }
+            if (key === '__vue_app__') {
+                return '[VueApp]'; // Indicate Vue app
+            }
             if (typeof value === 'object' && value !== null) {
                 if (seen.has(value)) {
                     return '[Circular]'; // Indicate circular reference
@@ -147,15 +287,25 @@ async fn main() -> Result<()> {
     console.log(JSON.stringify(root, circularReplacer(), 2));
 "#;
 
-        let vue_init_code =
-            v8::String::new(scope, vue_init_code).wrap_err("Failed to create JS string")?;
-        let script =
-            v8::Script::compile(scope, vue_init_code, None).wrap_err("Failed to compile script")?;
-        let result = script.run(scope).wrap_err("Failed to run script")?;
-        let result = result
-            .to_string(scope)
-            .wrap_err("Failed to convert result to string")?;
-        println!("result (app init): {}", result.to_rust_string_lossy(scope));
+            let vue_init_code =
+                v8::String::new(scope, vue_init_code).wrap_err("Failed to create JS string")?;
+            let script = v8::Script::compile(scope, vue_init_code, None)
+                .wrap_err("Failed to compile script")?;
+            let result = script.run(scope).wrap_err("Failed to run script")?;
+            let result = result
+                .to_string(scope)
+                .wrap_err("Failed to convert result to string")?;
+            println!("result (app init): {}", result.to_rust_string_lossy(scope));
+
+            // check for background tasks
+            let has_tasks = scope.has_pending_background_tasks();
+            println!("Has pending background tasks: {}", has_tasks);
+            // run the promise microtasks
+            for _ in 0..10 {
+                println!("Running microtasks...");
+                scope.perform_microtask_checkpoint();
+            }
+        }
     }
 
     let native_options = eframe::NativeOptions::default();
